278001ac3a64313d91e2087175b19c80ba603592
Melody Song <songxueyang94@gmail.com>
2026-02-22 20:46:00 -0800

    pages deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 900,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          charts: ['recharts'],
+        },
+      },
+    },
+  },
 });
